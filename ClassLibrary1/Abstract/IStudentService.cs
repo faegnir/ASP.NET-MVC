a3d98e2ee3bb4c29e282b34af2d98b0af7bc22c2
faegnir <71670295+faegnir@users.noreply.github.com>
2022-12-21 00:15:33 +0300

    businessLayer

---
+++ ClassLibrary1/Abstract/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IStudentService
+    {
+        void AddStudent(Student student);
+        void UpdateStudent(Student student);
+        void DeleteStudent(Student student);
+        List<Student> GetAllStudents();
+        Student GetStudentByID(int id);
+    }
+}
---
+++ MVC_App/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_App.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentManager studentManager = new StudentManager(new EfStudentRepository());
+        public IActionResult Index()
+        {
+            var values = studentManager.GetAllStudents();
+
+            return View(values);
+        }
+    }
+}
